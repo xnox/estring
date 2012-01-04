e053ba9f5c84b7fc46f6cf4c5630564eb4305223
Jeremie Dimino <jeremie@dimino.org>
2012-01-04 15:11:43 +0100

    remove oasis files
    Ignore-this: be1803f10ca9f144b521de454156e02
    
    darcs-hash:20120104141143-c41ad-2b748cc12b756fb4ee1fe9d93511fd6f30b3ef81

--- predist.sh
+++ predist.sh
@@ -3,14 +3,8 @@
 # Generate CHANGES.darcs
 [ -d "$DARCS_REPO" ] && darcs changes --repodir "$DARCS_REPO" > CHANGES.darcs
 
-# Let oasis generate a standard Makefile
-rm -f Makefile
-
 # Add oasis stuff
 oasis setup
 
-# Make the configure script executable
-chmod +x configure
-
 # Cleanup
-rm -f predist.sh boring
+rm -f dist.sh predist.sh boring
