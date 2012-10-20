4b06ddc408093d79e8c3a3319cee79f321b80b46
Jérémie Dimino <jeremie@dimino.org>
2012-10-20 11:51:41 +0200

    Add a Makefile
    It compiles setup.ml to a native executable.

--- setup.ml
+++ setup.ml
@@ -3,12 +3,9 @@
  * --------
  * Copyright : (c) 2012, Jeremie Dimino <jeremie@dimino.org>
  * Licence   : BSD3
- *
- * This file is a part of estring.
  *)
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
 #use "topfind";;
 #require "oasis.dynrun";;
 open OASISDynRun;;
